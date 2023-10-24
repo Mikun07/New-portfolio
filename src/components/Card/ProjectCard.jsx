0542fabc26dae22780056e3fd7792a292dc18c0d
Mikun07 <ayomikunolaleye@gmail.com>
2023-10-24 22:09:42 +0100

    Project Card Fix

--- src/components/Card/ProjectCard.jsx
+++ src/components/Card/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function ProjectCard({Img, Href, Title, SubTitle, Info}) {
+function ProjectCard({ Img, Href, Title, SubTitle, Info }) {
   return (
     <>
       <article className="px-6 py-4 flex flex-col gap-4 bg-white rounded-lg">
@@ -8,20 +8,15 @@ function ProjectCard({Img, Href, Title, SubTitle, Info}) {
           <img src={Img} alt="" className="w-full" />
           <div className=" absolute w-full h-full bg-black/20 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
             <button className="cursor-pointer text-orange-500 bg-transparent w-[50px] h-[50px] border-2 border-orange-500 rounded-full">
-              <a
-                target="_blank"
-                href={Href}
-              >
+              <a target="_blank" href={Href}>
                 <ion-icon name="logo-github" size="large"></ion-icon>
               </a>
             </button>
           </div>
         </div>
         <div className="">
-          <h2 className=" capitalize font-bold">{Title}</h2>
-          <h3 className="capitalize font-medium">
-            {SubTitle}
-          </h3>
+          <h2 className=" capitalize font-bold">{SubTitle}</h2>
+          <h3 className="capitalize font-medium">{Title}</h3>
 
           <p className="text-justify font-normal leading-6 tracking-wide">
             {Info}
