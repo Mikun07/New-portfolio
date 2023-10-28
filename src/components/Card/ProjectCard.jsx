ff039b058f57a314a900544fb61e636c1e84ded8
Mikun07 <ayomikunolaleye@gmail.com>
2023-10-29 00:01:55 +0100

    Project Page Fix

--- src/components/Card/ProjectCard.jsx
+++ src/components/Card/ProjectCard.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function ProjectCard({ Img, Href, Title, SubTitle, Info }) {
   return (
     <>
-      <article className="px-6 py-4 flex flex-col gap-4 bg-white rounded-lg">
+      <article className="lg:w-[360px] w-[300px] h-[380px] flex flex-col gap-4 bg-white rounded-lg">
         <div className="relative overflow-hidden group">
-          <img src={Img} alt="" className="w-full" />
-          <div className=" absolute w-full h-full bg-black/20 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
+          <img src={Img} alt="" className="lg:w-[360px] w-[300px] rounded-t-lg" />
+          <div className=" absolute lg:w-[360px] w-[300px] h-full bg-black/75 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
             <button className="cursor-pointer text-white bg-transparent w-[50px] h-[50px] border-2 border-white rounded-full">
               <a target="_blank" href={Href}>
                 <ion-icon name="logo-github" size="large"></ion-icon>
@@ -14,11 +14,11 @@ function ProjectCard({ Img, Href, Title, SubTitle, Info }) {
             </button>
           </div>
         </div>
-        <div className="">
-          <h2 className="capitalize text-sm font-bold section__title-1">{SubTitle}</h2>
-          <h3 className="capitalize font-bold">{Title}</h3>
+        <div className=" lg:w-[360px] w-[300px] flex flex-col gap-2 px-4 pb-3">
+          <p className="capitalize font-bold text-xs section__title-2">{SubTitle}</p>
+          <p className="capitalize text-sm font-bold">{Title}</p>
 
-          <p className="text-justify font-normal leading-5 tracking-tight">
+          <p className="text-justify font-normal leading-6 tracking-tighter">
             {Info}
           </p>
         </div>
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Project1 from "../../assets/img/project-1.jpg";
 import Project2 from "../../assets/img/project-2.jpg";
 import Project3 from "../../assets/img/project-3.jpg";
 import ProjectCard from "../../components/Card/ProjectCard";
+import SeeMoreProjectModal from "../../components/Modal/SeeMoreProjectModal";
 
 function Projects() {
+  const [showModel, setShowModal] = useState(false);
+  const handleOnClose = () => setShowModal(false);
+
   return (
     <>
       <div
         name="project"
-        className="lg:h-screen lg:pt-0 md:pt-[550px] sm:pt-[550px] pt-[450px] lg:px-20 px-4 flex items-center justify-center bg-gray-200"
+        className="lg:h-screen lg:pt-[85px] md:pt-[550px] sm:pt-[550px] pt-[450px] lg:px-20 px-4 flex items-center justify-center bg-gray-200"
       >
         <div className="flex flex-col">
           <div className=" flex justify-center items-center">
@@ -18,31 +22,44 @@ function Projects() {
             </h1>
           </div>
 
-          <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-6">
-            <ProjectCard
-              Img={Project3}
-              Href="https://festus-olaleye-ayomikun.netlify.app"
-              Title="Website"
-              SubTitle="Portfolio Website."
-              Info="This portfolio is a beautifully designed, modern, and responsive website built using ReactJS and Tailwind CSS."
-            />
-            <ProjectCard
-              Img={Project2}
-              Href="https://mimabooking.netlify.app"
-              Title="Website"
-              SubTitle="Hotel Booking Dashboard."
-              Info=" This is a beautifully designed hotel booking dashboard built
-                  using ReactJS and Tailwind CSS."
-            />
-            <ProjectCard
-              Img={Project1}
-              Href="mikun-films.netlify.app"
-              Title="Website"
-              SubTitle="Movie Recommendation website."
-              Info=" This project recommend movies bases on genre using ReactJS
-              with Redux for state management and local storage for data
-              persistence and Tailwind CSS for styling."
-            />
+          <div className="p-2 ">
+            <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-4">
+              <ProjectCard
+                Img={Project3}
+                Href="https://festus-olaleye-ayomikun.netlify.app"
+                Title="Website"
+                SubTitle="Portfolio Website."
+                Info="A modern, responsive portfolio website, elegantly crafted with ReactJS and enhanced by Tailwind CSS design."
+              />
+              <ProjectCard
+                Img={Project2}
+                Href="https://mimabooking.netlify.app"
+                Title="Website"
+                SubTitle="Hotel Booking Dashboard."
+                Info="A stunning hotel booking dashboard crafted with ReactJS and enhanced by Tailwind CSS design."
+              />
+              <ProjectCard
+                Img={Project1}
+                Href="https://mikun-films.netlify.app"
+                Title="Website"
+                SubTitle="Movie Recommendation website."
+                Info="Movie recommendations by genre via ReactJS, state management with Redux, data persistence in local storage, and Tailwind CSS styling."
+              />
+            </div>
+
+            <div className="flex justify-center items-end my-3">
+              <button
+                onClick={() => setShowModal(true)}
+                className="cursor-pointer font-bold capitalize hover:scale-110 duration-300"
+              >
+                See More
+              </button>
+
+              <SeeMoreProjectModal
+                onClose={handleOnClose}
+                visible={showModel}
+              />
+            </div>
           </div>
         </div>
       </div>
